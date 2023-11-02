96b508153ccdfbee5de327f2ad9da0e2d291d03f
jaehuru <kimbs0516@gmail.com>
2023-11-03 04:17:45 +0900

    Lec_11

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -37,6 +37,12 @@ void AShooterCharacter::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("bool byBool: %d"), byBool);
 	
 	UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f, bool: %d"),myInt, myFloat, false);
+
+	FString myString{TEXT("My String!!!!")};
+	UE_LOG(LogTemp, Warning, TEXT("FString myString: %s"), *myString);
+
+	UE_LOG(LogTemp, Warning, TEXT("Name of instance: %s"), *GetName());
+	
 }
 
 // Called every frame
