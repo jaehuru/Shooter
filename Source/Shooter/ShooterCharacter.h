580ddeb40214b234edd4e9920d150ce621c06a52
jaehuru <kimbs0516@gmail.com>
2023-11-03 06:07:19 +0900

    Lec_17

--- Source/Shooter/ShooterCharacter.h
+++ Source/Shooter/ShooterCharacter.h
@@ -24,6 +24,19 @@ protected:
 
 	// Called for side to side input
 	void MoveRight(float Value);
+
+	/*
+	 * Called via input to turn at a given rate.
+	 * @param Rate  This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/*
+	 * Called via input to look up/down at a given rate.
+	 * @param Rate  This is a normalized rate, i.e. 1.0 means 100% of desired rate
+	 */
+	void LookUpAtRate(float Rate);
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -36,8 +49,18 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class USpringArmComponent* CameraBoom;
 
+	// Camera that follows the character
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollwCamera;
+
+	// Base turn rate, in deg/sec. Other scaling may affect final turn rate
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera,  meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	// Bse look up/dawn rate, in deg/sec. Other scaling may affect final turn rate
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera,  meta = (AllowPrivateAccess = "true"))
+	float BaseLookUpRate;
+	
 
 public:
 	// Returns CameraBoom subobject
