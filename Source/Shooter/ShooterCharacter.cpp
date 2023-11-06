b05d904535e62fd262ad5a6d8bc1a4be694b1be5
jaehuru <kimbs0516@gmail.com>
2023-11-06 16:46:12 +0900

    Lec_34

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -20,6 +20,7 @@ AShooterCharacter::AShooterCharacter() :
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
 	// Create a camera boom (pulls in towards the character if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
