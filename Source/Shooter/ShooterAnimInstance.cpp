37958058fc1b7a265e3ffcc83ce614f430c8f65e
jaehuru <kimbs0516@gmail.com>
2023-11-09 19:32:34 +0900

    Lec_38

--- Source/Shooter/ShooterAnimInstance.cpp
+++ Source/Shooter/ShooterAnimInstance.cpp
@@ -4,6 +4,7 @@
 #include "ShooterAnimInstance.h"
 #include "ShooterCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
 {
@@ -31,9 +32,34 @@ void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
 		{
 			bIsAccelerating = false;
 		}
+
+		FRotator AnimRotation = ShooterCharacter->GetBaseAimRotation();
+		FRotator MovementRotation =
+			UKismetMathLibrary::MakeRotFromX(
+				ShooterCharacter->GetVelocity());
+		MovementOffset = UKismetMathLibrary::NormalizedDeltaRotator(
+			MovementRotation,
+			AnimRotation).Yaw;
 		
+		/*FString RotationMessage = FString::Printf(
+			TEXT("Base Aim Rotation: %f"),
+			AnimRotation.Yaw);*/
+		
+		/*FString MovementRotationMessage =
+			FString::Printf(
+				TEXT("Movement Rotation: %f"),
+				MovementRotation.Yaw);*/
+		
+		/*FString OffsetMessage =
+			FString::Printf(
+			TEXT("Movement Offset Yaw: %f"),
+			AnimRotation.Yaw);*/
+		
+		/*if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(1, 0.f, FColor::White, OffsetMessage);
+		}*/
 	}
-	
 }
 
 void UShooterAnimInstance::NativeInitializeAnimation()
