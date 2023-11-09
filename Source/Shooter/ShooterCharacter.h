587d8a499b834c9000ddfa5ad317fbdd184b65b6
jaehuru <kimbs0516@gmail.com>
2023-11-09 19:08:13 +0900

    Lec_37

--- Source/Shooter/ShooterCharacter.h
+++ Source/Shooter/ShooterCharacter.h
@@ -39,6 +39,8 @@ protected:
 
 	// Called when the Fire Button is pressed
 	void FireWeapon();
+
+	bool GetBeamEndLocation(const FVector& MuzzleSocketLocation, FVector& OutBeamLocation);
 	
 public:	
 	// Called every frame
