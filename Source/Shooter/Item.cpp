43e13ad99b66278237f4dc262659c5a26eee0193
jaehuru <kimbs0516@gmail.com>
2023-11-25 08:14:54 +0900

    Lec_62

--- Source/Shooter/Item.cpp
+++ Source/Shooter/Item.cpp
@@ -4,6 +4,7 @@
 #include "Item.h"
 
 #include "Components/BoxComponent.h"
+#include "Components/WidgetComponent.h"
 
 // Sets default values
 AItem::AItem()
@@ -16,7 +17,9 @@ AItem::AItem()
 
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
 	CollisionBox->SetupAttachment(ItemMesh);
-	
+
+	PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	PickupWidget->SetupAttachment(GetRootComponent());
 }
 
 // Called when the game starts or when spawned
