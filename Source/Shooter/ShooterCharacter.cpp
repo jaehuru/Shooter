a7349fe98251e0607d1eb34b4bce1e6460165419
jaehuru <kimbs0516@gmail.com>
2023-11-26 21:12:25 +0900

    Lec_66

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -441,7 +441,26 @@ void AShooterCharacter::TraceForItems()
 				// Show Item's Pickup Widget
 				HitItem->GetPickupWidget()->SetVisibility(true);
 			}
+
+			// We hit an AItem lastframe
+			if (TraceHitItemLastFrame)
+			{
+				if (HitItem != TraceHitItemLastFrame)
+				{
+					// We are hitting a different AItem this frame from last frame or AIrem is null
+					TraceHitItemLastFrame->GetPickupWidget()->SetVisibility(false);
+				}
+				
+			}
+
+			// Store a reference to HitItem for nest frame
+			TraceHitItemLastFrame = HitItem;
 		}
+	}
+	else if (TraceHitItemLastFrame)
+	{
+		// No longer overlapping any items, Item last frame should not show widget
+		TraceHitItemLastFrame->GetPickupWidget()->SetVisibility(false);
 	}
 }
 
--- Source/Shooter/ShooterCharacter.h
+++ Source/Shooter/ShooterCharacter.h
@@ -219,6 +219,10 @@ private:
 
 	// Number of overlapped AItems
 	int8 OverlappedItemCount;
+
+	// The AItem we hit last frame
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Items, meta = (AllowPrivateAccess = true))
+	class AItem* TraceHitItemLastFrame;
 	
 	float ShootTimeDuration;
 	
