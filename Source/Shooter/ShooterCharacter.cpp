ae1785faedcb6b41f995abf06dd7b9c78be497ac
jaehuru <kimbs0516@gmail.com>
2023-12-02 02:19:56 +0900

    Lec_77

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -507,6 +507,7 @@ void AShooterCharacter::DropWeapon()
 		EquippedWeapon->GetItemMesh()->DetachFromComponent(DetachmentTransformRules);
 
 		EquippedWeapon->SetItemState(EItemState::EIS_Falling);
+		EquippedWeapon->ThrowWeapon();
 	}
 }
 
