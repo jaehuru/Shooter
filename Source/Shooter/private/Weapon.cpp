d429c2b4435b07733ad1debba5b7adb8559b79df
jaehuru <kimbs0516@gmail.com>
2024-06-17 18:38:15 +0900

    Lec_211

--- Source/Shooter/private/Weapon.cpp
+++ Source/Shooter/private/Weapon.cpp
@@ -120,6 +120,11 @@ void AWeapon::OnConstruction(const FTransform& Transform)
 			SetClipBoneName(WeaponDataRow->ClipBoneName);
 			SetReloadMontageSection(WeaponDataRow->ReloadMontageSection);
 			GetItemMesh()->SetAnimInstanceClass(WeaponDataRow->AnimBP);
+			CrosshairsMiddle = WeaponDataRow->CrosshairsMiddle;
+			CrosshairsLeft = WeaponDataRow->CrosshairsLeft;
+			CrosshairsRight = WeaponDataRow->CrosshairsRight;
+			CrosshairsBottom = WeaponDataRow->CrosshairsBottom;
+			CrosshairsTop = WeaponDataRow->CrosshairsTop;
 		}
 
 		if (GetMaterialInstance())
--- Source/Shooter/public/Weapon.h
+++ Source/Shooter/public/Weapon.h
@@ -55,6 +55,21 @@ struct FWeaponDataTable : public FTableRowBase
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TSubclassOf<UAnimInstance> AnimBP;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* CrosshairsMiddle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* CrosshairsLeft;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* CrosshairsRight;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* CrosshairsBottom;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* CrosshairsTop;
 };
 
 /**
@@ -113,6 +128,22 @@ private:
 	UDataTable* WeaponDataTable;
 
 	int32 PreviousMaterialIndex;
+
+	// Textures for the weapon crosshairs
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = DataTable, meta = (AllowPrivateAccess = true))
+	UTexture2D* CrosshairsMiddle;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = DataTable, meta = (AllowPrivateAccess = true))
+	UTexture2D* CrosshairsLeft;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = DataTable, meta = (AllowPrivateAccess = true))
+	UTexture2D* CrosshairsRight;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = DataTable, meta = (AllowPrivateAccess = true))
+	UTexture2D* CrosshairsBottom;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = DataTable, meta = (AllowPrivateAccess = true))
+	UTexture2D* CrosshairsTop;
 	
 public:
 
@@ -131,11 +162,9 @@ public:
 
 	// Called from Character class when firing weapon
 	void DecrementAmmo();
-
-
+	
 	void ReloadAmmo(int32 Amount);
-
-
+	
 	bool ClipIsFull();
 	
 };
